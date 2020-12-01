21e25b35c577f67b501f56ba040caaa735bc967d
Ari Blondal <ari.blondal@gmail.com>
2020-11-30 23:58:27 -0800

    Precommit app.py

--- app.py
+++ app.py
@@ -1,8 +1,20 @@
 from flask import Flask, escape, request, render_template, send_from_directory
+from flask_sqlalchemy import SQLAlchemy
 from time import sleep
 
 app = Flask(__name__,
     static_folder='static/')
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+dp = SQLAlchemy(app)
+
+class Dataentry(db.Model):
+    __tablename__ = "games"
+    id = db.Column(db.Integer, primary_key=True) # Game id
+    turn = db.Column(db.SmallInteger) # 1 for red, 2 for blue
+    boardState = db.Column(db.String(64))
+
+
 
 @app.route('/make_move', methods=['POST'])
 def make_move():
